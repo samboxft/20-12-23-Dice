cfa2c0186599188e372c4a7a39525841622e1f50
samboxft <samGreenLite@gmail.com>
2020-12-25 11:45:11 +0200

    flag move

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function rolldice() {
     var image2 = document.querySelectorAll("img")[1];
     image2.setAttribute("src", randomImageSource2);
     if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").innerHTML = "player 1 Wins 🚩";
+        document.querySelector("h1").innerHTML = "🚩player 1 Wins ";
     }
     else if (randomNumber2 > randomNumber1) {
-        document.querySelector("h1").innerHTML = "🚩player 2 Wins ";
+        document.querySelector("h1").innerHTML = "player 2 Wins 🚩";
     }
     else {
         document.querySelector("h1").innerHTML = "Draw";
